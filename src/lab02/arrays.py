f81be374d1f8350a66ff2efa5ec145b150997ae2
Kira-Glazunova <@Fufel.(none)>
2025-10-22 15:31:57 +0300

    "fdjhgf"

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -4,11 +4,21 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     else:
         return ((min(nums), max(nums)))
 
+print(min_max([10, 3, -100, 9, 2]))
+print(min_max([10]))
+print(min_max([-1, -11, -2]))
+print(min_max([]))
+print(min_max([1.4, -8.4, 5, 6.66]))
+
 
 def unique_sorted(nums: list[float | int]) -> list[float | int]:
     nums.sort()
     return [*set(nums)]
 
+print(unique_sorted([5, 1, 1, 2, 5]))
+print(unique_sorted([]))
+print(unique_sorted([-2, 0, -342, -2, 10]))
+print(unique_sorted([1.3, 1.2, 0, -4.5]))
 
 def flatten(mat: list[list | tuple]) -> list:
     answer = []
@@ -19,3 +29,8 @@ def flatten(mat: list[list | tuple]) -> list:
             for member in arr:
                 answer.append(member)
     return answer
+
+print(flatten([[10, 2], [1, 5]]))
+print(flatten([[10, 24], (11, 1, 5)]))
+print(flatten([[], [23], [1, 2]]))
+print(flatten(["sfggd", [10, 11, 120]]))
--- src/lab02/matrix.py
+++ src/lab02/matrix.py
@@ -11,7 +11,13 @@ def transpose(mat: list[list[float | int]]) -> list[list]:
             new_matrix[y][x] = mat[x][y]
     return new_matrix
 
+print(transpose([[4, 1, 5]]))
+print(transpose([[5], [10], [1]]))
+print(transpose([]))
+print(transpose([[11, 12], [9, 10]]))
+print(transpose([[1111], [1, 2, 3]]))
 
+print()
 def row_sums(mat: list[list[float | int]]) -> list[float]:
     if any(len(mat[0]) != len(mat[i]) for i in range(len(mat))):
         return 'ValueError'
@@ -24,7 +30,12 @@ def row_sums(mat: list[list[float | int]]) -> list[float]:
         summa.append(sum(mat[arr]))
     return summa
 
+print(row_sums([[100, 1, 12], [1, 2, 3]]))
+print(row_sums([[-10, 0], [-9, 1]]))
+print(row_sums([[0, 0, 0], [0, 0, 0]]))
+print(row_sums([[1, 2], [3]]))
 
+print()
 def col_sums(mat: list[list[float | int]]) -> list[float]:
     if any(len(mat[0]) != len(mat[i]) for i in range(len(mat))):
         return 'ValueError'
@@ -38,3 +49,7 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
             summa[y] += (mat[x][y])
     return summa
 
+print(col_sums([[2, 3, 6], [1, 4, 5]]))
+print(col_sums([[-1000, 10], [-1, -4]]))
+print(col_sums([[0], [0]]))
+print(col_sums([[1], [5, 8, 0]]))
